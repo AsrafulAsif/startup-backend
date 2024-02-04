c78e12ed3f06ee9b2ddddcbf85b190c7884de3d4
asraful.asif <asifasrafulislam@gmail.com>
2024-02-04 11:02:41 +0600

    object string convertion added2

--- src/main/java/com/example/startup/startup/utils/ConvertingObjectStringObject.java
+++ src/main/java/com/example/startup/startup/utils/ConvertingObjectStringObject.java
@@ -4,13 +4,21 @@ import com.example.startup.startup.exception.JsonConversionException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConvertingObjectToString {
+public class ConvertingObjectStringObject {
     private static final ObjectMapper objectMapper = new ObjectMapper();
     public static String  convertObjectToString(Object object) {
         try {
             return objectMapper.writeValueAsString(object);
         } catch (JsonProcessingException e) {
-            throw new JsonConversionException(e.getOriginalMessage());
+            throw new JsonConversionException(e.getMessage());
+        }
+    }
+
+    public static <T> T  convertStringToObject(String data,Class<T> classType) {
+        try {
+            return objectMapper.readValue(data, classType);
+        } catch (JsonProcessingException e) {
+            throw new JsonConversionException(e.getMessage());
         }
     }
 }
